83f8b1e369f0970a91db789770156712b792f6b7
KS <wyangsun@163.com>
2015-07-28 00:25:36 +0800

    Update bhpnet.py

--- bhpnet.py
+++ bhpnet.py
@@ -30,59 +30,6 @@ def usage():
     print """echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135 """
     sys.exit(0)
 
-def main():
-    global listen
-    global port
-    global execute
-    global command
-    global upload_destination
-    global target
-
-    if not len(sys.argv[1:]):
-      usage()
-    
-    # read the commandline options
-    try:
-        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:",["help","listen","execute","target","port","command","upload"])
-    except getopt.GetoptError as err:
-        print str(err)
-        usage()
-
-    for o,a in opts:
-        if o in ("-h","--help"):
-            usage()
-        elif o in ("-l","--listen"):
-            listen = True
-        elif o in ("-e","--execute"):
-            execute = a
-        elif o in ("-c","--commandshell"):
-            command = True
-        elif o in ("-u","--upload"):
-            upload_destination = a
-        elif o in ("-t","--target"):
-            target = a
-        elif o in ("-p", "--port"):
-            port = int(a)
-        else:
-            assert False,"Unhandled Option"
-
-    # are we going to listen or just send data from stdin?
-    if not listen and len(target) and port > 0:
-
-        # read in the buffer from the commandline
-        # this will block, so send CTRL-D if not sending input
-        # to stdin
-        buffer = sys.stdin.read()
-
-        # send data off
-        client_sender(buffer)
-    
-    # we are going to listen and potentially
-    # upload things, execute commands, and drop a shell back
-    # depending on our command line options above
-    if listen:
-        server_loop()
-
 def client_sender(buffer):
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -204,4 +151,57 @@ def client_handler(client_socket):
       
       # send back the response
       client_socket.send(response)
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+      usage()
+    
+    # read the commandline options
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:",["help","listen","execute","target","port","command","upload"])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    for o,a in opts:
+        if o in ("-h","--help"):
+            usage()
+        elif o in ("-l","--listen"):
+            listen = True
+        elif o in ("-e","--execute"):
+            execute = a
+        elif o in ("-c","--commandshell"):
+            command = True
+        elif o in ("-u","--upload"):
+            upload_destination = a
+        elif o in ("-t","--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False,"Unhandled Option"
+
+    # are we going to listen or just send data from stdin?
+    if not listen and len(target) and port > 0:
+
+        # read in the buffer from the commandline
+        # this will block, so send CTRL-D if not sending input
+        # to stdin
+        buffer = sys.stdin.read()
+
+        # send data off
+        client_sender(buffer)
+    
+    # we are going to listen and potentially
+    # upload things, execute commands, and drop a shell back
+    # depending on our command line options above
+    if listen:
+        server_loop()
 main()
