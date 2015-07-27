73749f073891f4aabd0396c536a81ab306462ecc
KS <wyangsun@163.com>
2015-07-28 00:54:11 +0800

    Update bhpnet.py

--- bhpnet.py
+++ bhpnet.py
@@ -77,7 +77,7 @@ def server_loop():
     if not len(target):
         target = "0.0.0.0"
         
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((target,port))
     server.listen(5)
     
